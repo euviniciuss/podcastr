87bb98c05eca69043095433c578a8eda4a5ee6c2
dev-viniciuss <dev.mvsc@gmail.com>
2021-05-04 15:32:21 -0300

    feat: adcionando e implementando o axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,17 @@
-export default function Home(props) {  
+import { GetStaticProps } from 'next'
+import { api } from '../services/api'
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+}
+
+type HomeProps = {
+  episodes: Episodes[];
+}
+
+export default function Home(props: HomeProps) {  
   return (
     <div>
       <h1>Index</h1>
@@ -7,9 +20,14 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
 
   return {
     props: {
