100cfe7141b3cb0cae7d1869c1ffd413ed858243
dev-viniciuss <dev.mvsc@gmail.com>
2021-05-20 16:23:37 -0300

    feat: adicionando os parametros no GetStatichPaths

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -11,6 +11,7 @@ import { convertDurationToTimeString } from '../../utils/convertDurationToTimeSt
 
 import styles from './episode.module.scss'
 
+
 type Episode = {
   id: string;
   title: string;
@@ -63,8 +64,24 @@ export default function Episodes({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
